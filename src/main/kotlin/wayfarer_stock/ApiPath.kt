49f5cdd47d3bbfa9901809c703b6dc917662a420
Jhzlo <kjh0010703@naver.com>
2025-04-01 20:54:14 +0900

    refactor: api path 분리

---
+++ src/main/kotlin/wayfarer_stock/ApiPath.kt
@@ -0,0 +1,7 @@
+object ApiPath {
+    object Fabric {
+        const val REGISTER_FABRIC = "api/v1/fabric/register"
+        const val EDIT_FABRIC = "api/v1/fabric/{id}"
+        const val DELETE_FABRIC = "api/v1/fabric/{id}"
+    }
+}
--- src/main/kotlin/wayfarer_stock/fabric/controller/FabricController.kt
+++ src/main/kotlin/wayfarer_stock/fabric/controller/FabricController.kt
@@ -1,5 +1,6 @@
 package wayfarer_stock.fabric.controller
 
+import ApiPath
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -8,14 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import wayfarer_stock.fabric.application.FabricInfoFacade
 import wayfarer_stock.fabric.controller.dto.FabricInfoRequest
 
 @Tag(name = "Fabric", description = "Fabric Info API")
 @RestController
-@RequestMapping("/api/v1/fabric")
 class FabricController(
     private val fabricInfoFacade: FabricInfoFacade,
 ) {
@@ -23,7 +22,7 @@ class FabricController(
         summary = "원단 등록",
         description = "새로운 원단 정보를 등록한다."
     )
-    @PostMapping("/register")
+    @PostMapping(ApiPath.Fabric.REGISTER_FABRIC)
     fun registerFabric(
         @Parameter(description = "등록할 원단 정보")
         @RequestBody req: FabricInfoRequest
@@ -35,7 +34,7 @@ class FabricController(
         summary = "원단 수정",
         description = "기존의 원단 정보를 수정한다."
     )
-    @PutMapping("/{id}")
+    @PutMapping(ApiPath.Fabric.EDIT_FABRIC)
     fun updateFabric(
         @PathVariable id: Long,
         @Parameter(description = "수정할 원단 정보")
@@ -48,7 +47,7 @@ class FabricController(
         summary = "원단 삭제",
         description = "기존의 원단 정보를 삭제한다."
     )
-    @DeleteMapping("/{id}")
+    @DeleteMapping(ApiPath.Fabric.DELETE_FABRIC)
     fun deleteFabric(
         @Parameter(description = "삭제할 원단 id")
         @PathVariable id: Long,
