c28c403748d61fdb304f6ba681c914bfdb615f6a
Jhzlo <kjh0010703@naver.com>
2025-04-05 17:58:00 +0900

    feat: paging data class 정의

---
+++ src/main/kotlin/wayfarer_stock/core/web/PagingResult.kt
@@ -0,0 +1,18 @@
+package wayfarer_stock.core.web
+
+import org.springframework.data.domain.Page
+
+data class PagingResult<T>(
+    val content: List<T>,
+    val totalPages: Int,
+    val isLast: Boolean,
+) {
+    companion object {
+        fun <T> from(page: Page<T>): PagingResult<T> =
+            PagingResult(
+                content = page.content,
+                totalPages = page.totalPages,
+                isLast = page.isLast
+            )
+    }
+}
