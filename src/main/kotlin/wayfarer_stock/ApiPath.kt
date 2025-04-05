90caaf8c9597fb7c1d6e80116fde94e9f1c6c0f4
Jhzlo <kjh0010703@naver.com>
2025-04-05 20:55:59 +0900

    feat: api path 정의

--- src/main/kotlin/wayfarer_stock/ApiPath.kt
+++ src/main/kotlin/wayfarer_stock/ApiPath.kt
@@ -1,7 +1,11 @@
 object ApiPath {
     object Fabric {
-        const val REGISTER_FABRIC = "api/v1/fabric/register"
-        const val EDIT_FABRIC = "api/v1/fabric/{id}"
-        const val DELETE_FABRIC = "api/v1/fabric/{id}"
+        const val REGISTER_FABRIC = "/api/v1/fabric/register"
+        const val READ_FABRIC_LIST = "/api/v1/fabric"
+        const val READ_DETAILED_FABRIC_INFO = "/api/v1/fabric/{id}"
+        const val READ_FABRIC_BY_VENDOR = "/api/v1/fabric/search/vendor"
+        const val READ_FABRIC_BY_TYPE = "/api/v1/fabric/search/type"
+        const val EDIT_FABRIC = "/api/v1/fabric/{id}"
+        const val DELETE_FABRIC = "/api/v1/fabric/{id}"
     }
 }
