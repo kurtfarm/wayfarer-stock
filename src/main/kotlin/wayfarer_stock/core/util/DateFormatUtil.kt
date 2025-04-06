bef4521d6a4ba9897e111ab848d08163664f77c2
Jhzlo <kjh0010703@naver.com>
2025-04-06 13:41:02 +0900

    refactor: date formatter locale 사용

--- src/main/kotlin/wayfarer_stock/core/util/DateFormatUtil.kt
+++ src/main/kotlin/wayfarer_stock/core/util/DateFormatUtil.kt
@@ -2,28 +2,15 @@ package wayfarer_stock.core.util
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 object DateFormatUtil {
 
     private val dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
-    private fun getKoreanDayOfWeek(date: LocalDate): String {
-        val dayOfWeek = date.dayOfWeek
-        return when (dayOfWeek) {
-            java.time.DayOfWeek.MONDAY -> "월"
-            java.time.DayOfWeek.TUESDAY -> "화"
-            java.time.DayOfWeek.WEDNESDAY -> "수"
-            java.time.DayOfWeek.THURSDAY -> "목"
-            java.time.DayOfWeek.FRIDAY -> "금"
-            java.time.DayOfWeek.SATURDAY -> "토"
-            java.time.DayOfWeek.SUNDAY -> "일"
-        }
-    }
+    private val getKoreanDateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd (E)", Locale.KOREAN)
 
     fun formatWithDayOfWeek(date: LocalDate): String {
-        val formattedDate = date.format(dateFormatter)
-        val dayOfWeek = getKoreanDayOfWeek(date)
-        return "$formattedDate($dayOfWeek)"
+        return date.format(getKoreanDateFormatter)
     }
 
     fun formatDate(date: LocalDate): String{
